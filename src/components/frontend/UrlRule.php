c02c871e1c2a52e14aea67f12d52fe72cbcc1414
andrey <6097033725>
2019-08-24 09:18:28 +0300

    misc

--- src/components/frontend/UrlRule.php
+++ src/components/frontend/UrlRule.php
@@ -43,11 +43,11 @@ class UrlRule extends BaseObject implements UrlRuleInterface
 		$pathInfo = $request->getPathInfo();
 		if (preg_match('%^([\w_-]+)(?:\/([\w_-]+))?$%', $pathInfo, $matches)) {
 			$category_slug = $matches[1];
-			$category = Category::findOne(['slug' => $category_slug, 'is_main' => 0]);
+			$category = Category::findOne(['slug' => $category_slug]);
 			if ($category) {
 				if (isset($matches[2])) {
 					$page_slug = $matches[2];
-					$page = Page::findOne(['slug' => $page_slug, 'category_id' => $category->id, 'is_main' => 0]);
+					$page = Page::findOne(['slug' => $page_slug, 'is_main' => 0, 'category_id' => $category->id]);
 					if ($page) {
 						return ['custompages/default/page', ['id' => $page->id]];
 					}
@@ -57,7 +57,7 @@ class UrlRule extends BaseObject implements UrlRuleInterface
 			} else {
 				if (!isset($matches[2])) {
 					$page_slug = $matches[1];
-					$page = Page::findOne(['slug' => $page_slug, 'hide_category_slug' => 1, 'is_main' => 0]);
+					$page = Page::findOne(['slug' => $page_slug, 'is_main' => 0, 'hide_category_slug' => 1]);
 					if ($page) {
 						return ['custompages/default/page', ['id' => $page->id]];
 					}
