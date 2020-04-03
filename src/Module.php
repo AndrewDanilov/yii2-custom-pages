082d8eea1dcf0db2634d803ada8b0656616bf8c4
andrey <6097033725>
2020-04-03 22:27:18 +0300

    Misc

--- src/Module.php
+++ src/Module.php
@@ -70,7 +70,7 @@ class Module extends \yii\base\Module
 		Yii::$app->i18n->translations['custompages/*'] = [
 			'class'          => 'yii\i18n\PhpMessageSource',
 			'sourceLanguage' => 'en-US',
-			'basePath'       => '@andrewdanilov/messages',
+			'basePath'       => '@custompages/messages',
 			'fileMap'        => [
 				'custompages/backend' => 'backend.php',
 			],
