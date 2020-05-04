0dba6138d4574eee4196d7d0840dae4be21c08b4
andrey <6097033725>
2020-05-04 03:36:39 +0300

    Misc

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,19 +1,24 @@
 <?php
 namespace andrewdanilov\custompages;
 
+use Yii;
 use yii\base\BootstrapInterface;
 
 class Bootstrap implements BootstrapInterface
 {
+	public $appFrontendId = 'app-frontend';
+
 	/**
 	 * @inheritdoc
 	 */
 	public function bootstrap($app)
 	{
-		$app->getUrlManager()->addRules([
-			[
-				'class' => 'andrewdanilov\custompages\components\frontend\UrlRule',
-			]
-		], false);
+		if (Yii::$app->id === $this->appFrontendId) {
+			$app->getUrlManager()->addRules([
+				[
+					'class' => 'andrewdanilov\custompages\components\frontend\UrlRule',
+				]
+			], false);
+		}
 	}
 }
